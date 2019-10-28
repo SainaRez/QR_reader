d40899ef463b3785e422d24b8602d5bfa7d89c1e
Saina <saina.r6@gmail.com>
2019-10-28 12:52:56 -0400

    Changed the ai_flag to passive

--- src/server.cpp
+++ src/server.cpp
@@ -22,13 +22,11 @@ int main (int argc, char *argv[]) {
 
     hints.ai_family = AF_INET; // IPv4 (other option is IPv6) 
     hints.ai_socktype = SOCK_STREAM; // TCP (other optionis UDP)
-    hints.ai_flags = IPPROTO_TCP; //
+    hints.ai_flags = AI_PASSIVE;
     
+    // Get the network information (local IP: 127.0.0.1)
     int add = getaddrinfo(NULL, port, &hints, &result); // If result is populated, then the function returns 0
     if (add != 0){
-        std::cout << port << std::endl;
-        std::cout << add << std::endl;
-        std::cout << result->ai_addr << std::endl;
         std::cerr << "getAddrInfo failed" << " " << strerror(errno) << std::endl;
         return -1;
     }
@@ -43,7 +41,7 @@ int main (int argc, char *argv[]) {
     // Binding address to the created socket
     int bindFD = bind(sockFD, result->ai_addr, result->ai_addrlen);
     if (bindFD == -1){
-        std::cout << "Failure binding socket address and port number" << std::endl;
+        std::cout << "Failure binding socket address and port number" << " " << strerror(errno) << std::endl;
         return -1;
     }
 
